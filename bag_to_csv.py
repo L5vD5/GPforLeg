9aba84fd605269799715b2cc32224ff8addcdba8
L5vD5 <l5vd5@korea.ac.kr>
2022-05-25 09:05:53 +0900

    update bag to csv

--- bag_to_csv.py
+++ bag_to_csv.py
@@ -1,7 +1,17 @@
 import bagpy
 from bagpy import bagreader
+from os.path import isfile, join
+from os import listdir
 
-b = bagreader('./1450_L_001.bag')
-for t in b.topics:
-    data = b.message_by_topic(t)
-    print(data)
+ros_path = './ROSBAG'
+onlyfiles = [ros_path + '/' + f for f in listdir(ros_path) if isfile(join(ros_path, f))]
+for filename in onlyfiles:
+    print(filename)
+    b = None
+    try:
+        b = bagreader(filename)
+        for t in b.topics:
+            data = b.message_by_topic(t)
+            print(data)
+    except:
+        print('Error')
